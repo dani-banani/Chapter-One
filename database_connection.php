af3b5500922d074831e4b9bd32d650ccf5fa5c03
dani <danielmacdand@gmail.com>
2025-07-04 19:12:11 +0800

    database_connection.php added

---
+++ database_connection.php
@@ -0,0 +1,11 @@
+<?php
+$host = "noveldb.crusyaucwrfk.ap-southeast-1.rds.amazonaws.com";
+$port = 3306;
+$dbname = "noveldb";
+$username = "admin";
+$password = "password-NovelDB";
+$conn = new mysqli($host, $username, $password, $dbname, $port);
+if ($conn->connect_error) {
+    die("dbconnect failed" . $conn->connect_error);
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
+include 'database_connection.php';
 echo "Goodbye World"
 ?>
