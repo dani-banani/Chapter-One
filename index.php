ad378c1a5ded1f1dab38d1c93237185ed604082c
dani <danielmacdand@gmail.com>
2025-07-07 02:03:38 +0800

    format

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ include 'database_connection.php';
 echo "Goodbye World"
 ?>
 <html>
-    <a href="pages/login.php">Login</a>
-    <a href="pages/register.php">Register</a>
+    <a href="pages/login.html">Login</a>
+    <a href="pages/register.html">Register</a>
 </html>
